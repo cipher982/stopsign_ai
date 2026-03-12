be583701f478848e07becac1fc563073a0d48f92
David Rose <david@drose.io>
2026-03-11 23:15:48 -0300

    Disable remote playback on site videos

--- static/js/video-player.js
+++ static/js/video-player.js
@@ -6,6 +6,23 @@
 // Track initialization state to prevent multiple instances
 var videoPlayerInitialized = false;
 
+function disableRemotePlayback(video) {
+    if (!video) return;
+
+    video.setAttribute('playsinline', '');
+    video.setAttribute('controlslist', 'noremoteplayback');
+    video.setAttribute('disableremoteplayback', '');
+    video.setAttribute('x-webkit-airplay', 'deny');
+
+    if ('disableRemotePlayback' in video) {
+        video.disableRemotePlayback = true;
+    }
+
+    if (video.controlsList && typeof video.controlsList.add === 'function') {
+        video.controlsList.add('noremoteplayback');
+    }
+}
+
 function setStreamStatus(state) {
     var statusEl = document.getElementById('streamStatus');
     if (!statusEl) return;
@@ -70,6 +87,8 @@ function initializeVideoPlayer() {
         console.log('Video player element not found, will try again later');
         return;
     }
+
+    disableRemotePlayback(video);
 
     // Prevent multiple initializations
     if (videoPlayerInitialized || video.hlsInstance) {
